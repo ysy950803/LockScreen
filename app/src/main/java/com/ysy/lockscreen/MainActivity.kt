31de680ba5f7d7fdb3484977c689ebf85fe8b5a0
ysy950803 <admin@ysy950803.cn>
2018-05-15 12:57:42 +0800

    Add full-screen in black.

--- app/src/main/java/com/ysy/lockscreen/MainActivity.kt
+++ app/src/main/java/com/ysy/lockscreen/MainActivity.kt
@@ -1,43 +1,72 @@
 package com.ysy.lockscreen
 
+import android.annotation.TargetApi
 import android.app.Activity
 import android.app.admin.DevicePolicyManager
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
+import android.content.*
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.os.Process
+import android.provider.Settings
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
 
+    private val TAG = "MainActivty"
+    private val REQUEST_CODE_WRITE_SETTINGS = 1
+
     private lateinit var mPolicyManager: DevicePolicyManager
+
+    private var mOriginalLockTime: Int = 0
+    private var mIsNewLockMode: Boolean = false
+
     private var mComponentName: ComponentName? = null
+    private var mScreenStatReceiver: ScreenStatReceiver? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         init()
-        tryLockScreen()
         setContentView(R.layout.activity_main)
+        tryLockScreen()
     }
 
     private fun init() {
+        mIsNewLockMode = isNewLockMode()
         mPolicyManager = getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
         mComponentName = ComponentName(this, AdminReceiver::class.java)
+
+        if (mIsNewLockMode) {
+            mScreenStatReceiver = ScreenStatReceiver()
+            registerScreenStat()
+        }
     }
 
     private fun tryLockScreen() {
-        if (!mPolicyManager.isAdminActive(mComponentName)) {
-            activeManager()
+        if (mIsNewLockMode) {
+            if (canWriteSystem()) {
+                lockScreenDelayed()
+            } else {
+                requestWriteSettingsPerm()
+            }
         } else {
-            lockScreenNow()
+            if (!mPolicyManager.isAdminActive(mComponentName)) {
+                activeManager()
+            } else {
+                lockScreenNow()
+            }
+            killSelf()
         }
-        killSelf()
     }
 
     private fun activeManager() {
         val intent = Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN)
         intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, mComponentName)
-        intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "激活后即可使用一键锁屏哦(*^__^*)")
+        intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "激活后即可使用一键锁屏(*^__^*)")
         startActivity(intent)
     }
 
@@ -46,7 +75,88 @@ class MainActivity : Activity() {
     }
 
     private fun killSelf() {
+        unregisterScreenStat()
         finish()
         Process.killProcess(Process.myPid())
+    }
+
+    private fun isNewLockMode(): Boolean {
+        return Build.VERSION.SDK_INT >= 23
+    }
+
+    private fun canWriteSystem(): Boolean {
+        return Build.VERSION.SDK_INT >= 23 && Settings.System.canWrite(this)
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private fun requestWriteSettingsPerm() {
+        val intent = Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS)
+        intent.data = Uri.parse("package:$packageName")
+        startActivityForResult(intent, REQUEST_CODE_WRITE_SETTINGS)
+        Toast.makeText(this, "允许后按返回即可使用一键锁屏(*^__^*)", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_WRITE_SETTINGS && Build.VERSION.SDK_INT >= 23) {
+            if (Settings.System.canWrite(this)) {
+                lockScreenDelayed()
+            }
+        }
+    }
+
+    private fun lockScreenDelayed() {
+        blackScreen()
+        mOriginalLockTime = Settings.System.getInt(contentResolver, "screen_off_timeout")
+        Settings.System.putInt(contentResolver, "screen_off_timeout", 0)
+    }
+
+    private fun blackScreen() {
+        layout_main_bg.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+    }
+
+    private fun registerScreenStat() {
+        val filter = IntentFilter()
+        filter.addAction(Intent.ACTION_SCREEN_OFF)
+        filter.addAction(Intent.ACTION_SCREEN_ON)
+        filter.addAction(Intent.ACTION_USER_PRESENT)
+        this.registerReceiver(mScreenStatReceiver, filter)
+    }
+
+    private fun unregisterScreenStat() {
+        if (mScreenStatReceiver != null) {
+            this.unregisterReceiver(mScreenStatReceiver)
+        }
+    }
+
+    private inner class ScreenStatReceiver : BroadcastReceiver() {
+
+        override fun onReceive(context: Context?, intent: Intent?) {
+            when (intent!!.action) {
+                Intent.ACTION_SCREEN_OFF -> {
+                    Log.d(TAG, "Intent.ACTION_SCREEN_OFF")
+                    if (canWriteSystem()) {
+                        Settings.System.putInt(contentResolver, "screen_off_timeout", mOriginalLockTime)
+                    }
+                    Handler().postDelayed({
+                        killSelf()
+                    }, 768)
+                }
+                Intent.ACTION_SCREEN_ON -> {
+                    Log.d(TAG, "Intent.ACTION_SCREEN_ON")
+                    killSelf()
+                }
+                Intent.ACTION_USER_PRESENT -> {
+                    Log.d(TAG, "Intent.ACTION_USER_PRESENT")
+                    killSelf()
+                }
+            }
+        }
     }
 }
